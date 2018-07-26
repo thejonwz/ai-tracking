1d1ab67e6f5b86bf7a0159b700fc6a3ec49bab1f
Josborne3693 <38893476+Josborne3693@users.noreply.github.com>
2018-07-26 17:50:48 -0400

    Add files via upload

---
+++ stopTello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sleep 2s
+killall -9 telloGoVideo
---
+++ trackingScenario.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# move car
+# make sure BLDCtool is the last focused window
+python ~/moveCar.py forw 0.5
+
+# let the python script get the images for 25 seconds
+sleep 10s
+
+# kill the python process, freeing gpu
+killall -9 python
+
+# train model for tello
+
+# set up the kill
+./killTello.sh &
+
+# launch tello and let it do its thing
+cd /home/nvidia/swx/goStuff/telloGoVideo/
+./telloGoVideo
+
+./telloGoVideo
