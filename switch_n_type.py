0b06029f9676e638ade805c242b8df48bc5fe53a
Josborne3693 <josborne3693@floridapoly.edu>
2018-07-26 11:50:31 -0400

    added comments to switch_n_type

--- switch_n_type.py
+++ switch_n_type.py
@@ -1,13 +1,25 @@
+# Sofwerx ai-tracking RC Car controller script
+# To run this, in Terminal, run
+# python switch_n_type.py <direction> <time>
+# ex: python switch_n_type.py forw 0.5
+
+
+# import dependencies
 from pynput.keyboard import Key, Controller
 import time
 import sys
 
+# create Controller object
 keyboard = Controller()
 
+# get arguments, [1] is either 'forw' or 'back'
+# [2] decimal or int time to go in seconds
 toDo = sys.argv[1]
 howLong = float(sys.argv[2])
 print "Going " + toDo + " for " + str(howLong) + " seconds"
 
+# switches window focus to previously focused window
+# make sure the BLDC tool was the last focus
 def switchTabs():
 	keyboard.press(Key.alt)
 	keyboard.press(Key.tab)
@@ -16,6 +28,8 @@ def switchTabs():
 
 	time.sleep(0.5)
 
+# switches focus, presses key, waits, unpresses key, switches back to terminal
+# a time of 0.5 seconds makes the car travel ~5 feet
 def goForward(goTime):
 
 	switchTabs()
@@ -25,20 +39,19 @@ def goForward(goTime):
 
 	switchTabs()
 
+# switches focus, presses key, waits, unpresses key, switches back to terminal
+# back time needs to be a bit longer than forward time
 def goBackward(goTime):
 	switchTabs()
 
 	keyboard.press(Key.left)
 	time.sleep(goTime)
 	keyboard.release(Key.left)
-	
+
 	switchTabs()
 
+# forward or backward
 if toDo == 'forw':
 	goForward(howLong)
 elif toDo == 'back':
 	goBackward(howLong)
-
-#print(type(time))
-
-
